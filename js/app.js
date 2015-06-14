93cd09e4f2ec727ab7e521a5b062d18d647e6f42
abelalvarez89 <abelalvarez89@hotmail.com>
2015-06-14 04:18:56 -0400

    Second commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+(function(){
+var app = angular.module('expenses', []);
+
+app.controller("OutputController", function(){
+	this.product = outputObj;
+
+});
+	
+app.controller("LocationController", function(){
+
+this.productObj = expensesObject;
+
+this.getDate = function(){
+	this.productObj.showDate = true;
+};
+
+});
+
+app.controller('ReviewController', function() {
+  this.review = {};
+
+  this.addReview = function(product) {
+    outputObj.comments.push(product.date);
+  };
+});
+
+var expensesObject = {
+	showDate: false,
+
+	date: new Date(),
+
+	location: ['Chipotle','Home Depot',
+	'McDonald','Panda Express','Papa Johns',
+	'Pizza Hut','Publix','Shell','Subway',
+	'Walmart'
+	]	,
+	comments:[
+		
+		]
+};
+
+var outputObj = {
+	comments:[
+		
+		]
+};
+
+})();
