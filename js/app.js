98eac69342a6e8d061df1f54c56ee4b0a197b887
abelalvarez89 <abelalvarez89@hotmail.com>
2015-06-15 23:01:28 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,50 @@
 (function(){
 var app = angular.module('expenses', []);
 
-app.controller("OutputController", function(){
-	this.product = outputObj;
-});
-	
+
 app.controller("LocationController", function(){
 
 this.productObj = expensesObject;
 
 this.getDate = function(){
 	this.productObj.showDate = true;
+	this.productObj.date = new Date();
 };
 
 });
 
 app.controller('ReviewController', function() {
   this.review = {};
+  this.outputObj = localObj;
 
-  this.addReview = function(product) {
-    outputObj.comments.push(product.date);
+  
+  this.setValues = function(indexs, totals, dates){
+  	if(typeof(localStorage.indexing) == "undefined"){
+  		localStorage.indexing = 0;
+  		var t = [];
+  		localStorage["testing"] = JSON.stringify(t);
+  	}
+  	localStorage.store = indexs;
+  	localStorage.total = totals;
+  	localStorage.date = dates;
+  }
+
+    this.addReview = function(product) {
+    if(typeof(indexAt) == "undefined") {
+    	localStorage.indexAt = 0;
+    }
+  	this.outputObj.total = "Publix";
+  	var values = [localStorage.store + "%e", localStorage.total + "%e", localStorage.date + "%e"];
+
+  	var newArray = JSON.parse(localStorage["testing"]);
+  	localStorage["testing"] = JSON.stringify(newArray + "%n" + values);
+
+
+  	//stopping here. This will be used to look up completed trasactions
+  	var str = JSON.parse(localStorage["testing"]);
+
+  	var res = str.split("%n");
+  	
   };
 });
 
@@ -35,10 +60,11 @@ var expensesObject = {
 	]
 };
 
-var outputObj = {
-	comments:[
-		
-		]
-};
+var localObj = {
+	store: "",
+	total: ''
+
+}
+
 
 })();
